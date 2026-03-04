56f49624145cbcb2b8fd1997486aa48b7ae03796
kojuo32361537-ops <kojuo32361537@gmail.com>
2026-03-05 00:24:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once("ready", () => {
+  console.log("ログイン成功！");
+});
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "hello") {
+    await interaction.reply("こんにちは！");
+  }
+});
+
+client.login(process.env.TOKEN);
